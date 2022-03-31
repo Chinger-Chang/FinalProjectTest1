82d2867d9cf2b67d4fe2c7248ce05068215f2d8e
t108458055 <t108458055@ntut.edu.tw>
2022-03-31 16:31:06 +0800

    first code controller
    one connect object

--- FinalProjectFirstTest/FinalProjectFirstTest/Controllers/UserController.cs
+++ FinalProjectFirstTest/FinalProjectFirstTest/Controllers/UserController.cs
@@ -9,37 +9,57 @@ using System.Threading.Tasks;
 
 namespace FinalProjectFirstTest.Controllers
 {
+    //Buyer Controller
     public class UserController : Controller
     {
         private readonly List<User> _db;
+
+        //註冊 Registration Action
+
+        //註冊推送 Registration Post action
+
+
+        // 郵件認證 Verify Email
+
+        // 郵件認證連接 Verify Email LINK
+
+        //會員中心 Center
         public IActionResult UserCenter()
         {
             return View();
         }
 
-
+        //會員登入 Login
         public IActionResult UserLogin()
         {
             return View();
         }
+
+        //會員登出 Logout
+
+
+
+
+
         //測試
         [HttpPost]
-        public Boolean Usercheckin(UsersModel ssr)
+        public IActionResult Usercheckin(string account,string password)
 
         {
-            Console.WriteLine(ssr.Name);
-            return true;
+            Console.WriteLine($"登入帳號{account}密碼{password}");
+            
+            return Content($@"帳號{account}/密碼{password}");
         }
 
 
         public class UsersModel
 
         {
-            public string Name { get; set; }
+            public string account { get; set; }
 
-            public string Address { get; set; }
+            public string password { get; set; }
 
-            public string PhoneNo { get; set; }
+   
           
         }
 
