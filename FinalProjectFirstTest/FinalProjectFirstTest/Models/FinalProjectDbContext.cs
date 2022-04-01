09be47f511e30a9f2d3b04542817efd90d03b9c9
Chinger-Chang <62275922+Chinger-Chang@users.noreply.github.com>
2022-04-01 18:33:10 +0800

    Revert "Testn0"

--- FinalProjectFirstTest/FinalProjectFirstTest/Models/FinalProjectDbContext.cs
+++ FinalProjectFirstTest/FinalProjectFirstTest/Models/FinalProjectDbContext.cs
@@ -3,22 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-//規劃一個Persistence持久層框架(mapping 實體資料庫)
+
 namespace FinalProjectFirstTest.Models
 {
 	public class FinalProjectDbContext : DbContext
 	{
-		public FinalProjectDbContext()
-		{
-			Console.WriteLine("DbContext物件產生...!"); //確認物件是否共用?
-		}
-
-		//自訂建構子 注入 DbContext Option(選項)
 		public FinalProjectDbContext(DbContextOptions<FinalProjectDbContext> options) : base(options)
 		{
 
 		}
-		//定義自動屬性Property
+
 		public DbSet<User> Users { get; set; }
 
 		public DbSet<Service> Services { get; set; }
--- FinalProjectFirstTest/FinalProjectFirstTest/Program.cs
+++ FinalProjectFirstTest/FinalProjectFirstTest/Program.cs
@@ -11,10 +11,8 @@ namespace FinalProjectFirstTest
 {
 	public class Program
 	{
-		//Entry Point (應用程式進入點) Or Main Program(主程式)
 		public static void Main(string[] args)
 		{
-			//static method 只能呼叫static 成員(共用)
 			CreateHostBuilder(args).Build().Run();
 		}
 
@@ -22,10 +20,7 @@ namespace FinalProjectFirstTest
 			Host.CreateDefaultBuilder(args)
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
-					// 網站系統伺服器跟網站系統啟動...網站伺服器要用那些功能 ???
-					 //使用方法套用泛型Generic(插件)指定服務(Serivce)載入
-					 //與中介軟體(Middleware)啟用
-					 webBuilder.UseStartup<Startup>();
+					webBuilder.UseStartup<Startup>();
 				});
 	}
 }
